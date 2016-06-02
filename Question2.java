0fbf734cd1d001a61315b6b437fad4b98b92fe09
SAC-CS112-nguyen-kinh <kinhdinhnguyen12@yahoo.com>
2016-06-01 23:45:16 -0700

    Add files via upload

--- Question2.java
+++ Question2.java
@@ -1,5 +1,21 @@
 package final1;
 
 public class Question2 {
+	
+	public static void main(String[] args)
+	{
+		FlyingObject Bird = new FlyingObject(10,3);
+		FlyingObject Airplane = new FlyingObject(450,50);
+		FlyingObject JetPlane = new FlyingObject(1000, 120);
+		FlyingObject Missile = new FlyingObject(2500, 800);
+		FlyingObject Superman = new FlyingObject(3500,500);
+		
+		Bird.FlyingObject();
+		Airplane.FlyingObject();
+		JetPlane.FlyingObject();
+		Missile.FlyingObject();
+		Superman.FlyingObject();
+		
+	}
 
 }
--- SetClass.java
+++ SetClass.java
@@ -2,4 +2,52 @@ package final1;
 
 public class SetClass {
 
+	private int distanceInMile;
+	private int airSpeedMPH;
+	private int elevationPerMinute;
+	private int totalAltitude = 0;
+	
+	
+	public void FlyingObject (int airSpeedMPH, int elevationPerMinute)
+	{
+		this.airSpeedMPH = 0;
+		this.elevationPerMinute = 0;
+	}
+	
+	public void FlyingObject ()
+	{
+		this.airSpeedMPH = distanceInMile /60;
+		this.elevationPerMinute = elevationPerMinute;
+	}
+	
+	
+	public void SetAirSpeed(int airSpeedMPH)
+	{
+		this.airSpeedMPH = distanceInMile / 60 ;
+	}
+	
+	public void SetAltimeter(int elevationPerMinute)
+	{
+		this.elevationPerMinute = elevationPerMinute;
+	}
+	
+	public void Fly()
+	{
+		this.totalAltitude = this.totalAltitude + this.elevationPerMinute;
+		this.distanceInMile = this.distanceInMile + this.airSpeedMPH;
+	}
+	
+	public int GetAirSpeed()
+	{
+		return airSpeedMPH;
+	}
+	public int GetAltimeter()
+	{
+		return elevationPerMinute;
+	}
+	
+	public String toString()
+	{
+		return " " + this.distanceInMile + this.totalAltitude;
+	}
 }
